baa3006f07df116347fed07042b61d020a60cf24
Eric Bailey <eric@ericb.me>
2015-12-25 03:26:17 -0600

    Update parse_tests
    Add property tests for defun forms with pattern clauses, along with all
    the necessary types along the way.

--- test/parse_tests.erl
+++ test/parse_tests.erl
@@ -21,6 +21,10 @@ parse_test_() ->
        prop_defun_simple(), 100}
     , {"A simple function with a docstring is correctly parsed.",
        prop_defun_simple_doc(), 100}
+    , {"A function with pattern clauses produces an empty docstring.",
+       prop_defun_match(), 30}
+    , {"A function with pattern clauses and a docstring is correctly parsed.",
+       prop_defun_match_doc(), 30}
     ],
   [{timeout, ?TIMEOUT,
     {Title, ?_assert(proper:quickcheck(Property, ?OPTIONS(NumTests)))}}
@@ -45,6 +49,20 @@ prop_defun_simple_doc() ->
             lists:nth(4, Defun) =:=  Doc
           end).
 
+prop_defun_match() ->
+  ?FORALL(Defun, defun_match(),
+          begin
+            {ok, #{doc := Doc}} = 'lodox-parse':'form-doc'(Defun),
+            "" =:= Doc
+          end).
+
+prop_defun_match_doc() ->
+  ?FORALL(Defun, defun_match_doc(),
+          begin
+            {ok, #{doc := Doc}} = 'lodox-parse':'form-doc'(Defun),
+            lists:nth(3, Defun) =:=  Doc
+          end).
+
 
 %%%===================================================================
 %%% defun shapes
@@ -59,18 +77,65 @@ defun_simple_doc() ->
    docstring()
    | body()].
 
+defun_match() ->
+  [defun, atom()
+   | non_empty(list(pattern_clause()))].
+
+defun_match_doc() ->
+  [defun, atom(),
+   docstring()
+   | non_empty(list(pattern_clause()))].
+
 
 %%%===================================================================
 %%% Custom types
 %%%===================================================================
 
+%%% Functions
+
 arglist_simple() -> list(atom()).
 
 body() -> union([[printable_string()], [non_string_term() | list(form())]]).
 
+form() -> union([non_string_term(), printable_string(), [ atom() | list()]]).
+
 docstring() -> printable_string().
 
-form() -> union([non_string_term(), printable_string(), [ atom() | list()]]).
+arglist_patterns() -> non_empty(list(pattern())).
+
+
+%%% Patterns
+
+pattern() -> union([non_string_term(), printable_string(), pattern_form()]).
+
+pattern_form() ->
+  [oneof([backquote, quote, binary, list, map, tuple, match_fun()])
+   | non_empty(list())].
+
+match_fun() -> ?LET(F, printable_string(), list_to_atom("match-" ++ F)).
+
+pattern_clause() -> [arglist_patterns() | [oneof([guard(), form()]) | body()]].
+
+guard() -> ['when' | non_empty(list(union([logical_clause(), comparison()])))].
+
+
+%%% Logical clauses
+
+logical_clause() ->
+  X = union([atom(), comparison()]),
+  [logical_operator(), X | non_empty(list(X))].
+
+logical_operator() -> oneof(['and', 'andalso', 'or', 'orelse']).
+
+
+%%% Comparisons
+
+comparison() -> [comparison_operator(), atom() | list(atom())].
+
+comparison_operator() -> oneof(['==', '=:=', '=/=', '<', '>', '=<', '>=']).
+
+
+%%% Strings and non-strings
 
 non_string_term() ->
   union([atom(), number(), [], bitstring(), binary(), boolean(), tuple()]).
@@ -78,6 +143,7 @@ non_string_term() ->
 printable_char() -> union([integer(32, 126), integer(160, 255)]).
 
 printable_string() -> list(printable_char()).
+
 
 %%%===================================================================
 %%% Internal functions
